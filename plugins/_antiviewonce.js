c931f0f0d8d24c9f2fde8f8d10e6405ed47fb0a8
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-07 17:03:21 -0600

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,7 +1,8 @@
-let downloadContentFromMessage = (await import(global.baileys)).default
+let { downloadContentFromMessage } = (await import(global.baileys));
 
 export async function before(m, { isAdmin, isBotAdmin }) {
-let chat = db.data.chats[m.chat]
+ 
+let chat = db.data.chats[m.chat] 
 if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return
 if (!chat.antiver || chat.isBanned) return
 if (m.mtype == 'viewOnceMessageV2') {
@@ -12,7 +13,7 @@ let buffer = Buffer.from([])
 for await (const chunk of media) {
 buffer = Buffer.concat([buffer, chunk])}
 if (/video/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}` + lenguajeGB.smsAntiView1(), m)
+return this.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption}\n\n${lenguajeGB.smsAntiView()}`, m)
 } else if (/image/.test(type)) {
-return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}` + lenguajeGB.smsAntiView2(), m)
+return this.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption}\n\n${lenguajeGB.smsAntiView()}`, m)
 }}}
